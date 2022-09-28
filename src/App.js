572d6a88c10c209f682ff4ef8aa111429901748d
unknown <flffl1231@naver.com>
2022-09-28 17:37:04 +0900

    test

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { getMovieList } from "./api/naver";
+import { getBookList } from "./api/naver";
+function App() {
+  const headers = {
+    "X-Naver-Client-Id": "XRupn7GoMOzhZZQE_RD6",
+    "X-Naver-Client-Secret": "FxfJq0rU7f",
+  };
+
+  getMovieList();
+  getBookList();
+  return (
+    <div>
+      <h1>api test</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/api/naver.js
@@ -0,0 +1,22 @@
+const headers = {
+  "X-Naver-Client-Id": "XRupn7GoMOzhZZQE_RD6",
+  "X-Naver-Client-Secret": "FxfJq0rU7f",
+};
+
+export const getMovieList = async () => {
+  const response = await fetch("/v1/search/movie.json?query=1", {
+    headers: headers,
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
+};
+
+export const getBookList = async () => {
+  const response = await fetch("/v1/search/book.json?query=1", {
+    headers: headers,
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
+};
