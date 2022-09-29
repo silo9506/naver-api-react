b374e81944217c82df65bd888f55fef6fdb25b78
Eyeless356 <95523501+Eyeless356@users.noreply.github.com>
2022-09-29 12:36:30 +0900

    test01

--- src/api/naver.js
+++ src/api/naver.js
@@ -4,18 +4,24 @@ const headers = {
 };
 
 export const getMovieList = async () => {
-  const response = await fetch("/v1/search/movie.json?query=1", {
-    headers: headers,
-  })
+  const response = await fetch(
+    "https://cors-anywhere.herokuapp.com/https://openapi.naver.com/v1/search/movie.json?query=1",
+    {
+      headers: headers,
+    }
+  )
     .then((response) => response.json())
     .then((data) => console.log(data))
     .catch((error) => console.log(error));
 };
 
 export const getBookList = async () => {
-  const response = await fetch("/v1/search/book.json?query=1", {
-    headers: headers,
-  });
+  const response = await fetch(
+    "https://cors-anywhere.herokuapp.com/https://openapi.naver.com/v1/search/book.json?query=1",
+    {
+      headers: headers,
+    }
+  );
 
   let data = await response.json();
   console.log(data);
